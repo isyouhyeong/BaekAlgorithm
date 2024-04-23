1760b87e1bd9b09ecbbf4977efcd430747320b8d
isyouhyeong <162817850+isyouhyeong@users.noreply.github.com>
2024-04-23 09:22:12 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17976 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
+
+public class Main {
+    public static void main(String[] args) {
+        //두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
         Scanner sc = new Scanner(System.in);
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        System.out.print(A/B);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a / b);
     }
 }
