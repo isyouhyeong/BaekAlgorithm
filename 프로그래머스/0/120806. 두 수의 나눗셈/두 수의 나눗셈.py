84c5042c45deaf16ca8325c6cc04dbb9eed0f061
aiden_Lee <isyouh77@gmail.com>
2026-04-25 14:51:00 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.00 ms, Memory: 8.99 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.py
@@ -0,0 +1,10 @@
+# def solution(num1, num2):
+#     answer = 0
+    
+#     na = num1 / num2
+#     answer = round(na * 1000, 0)
+#     return answer
+
+
+def solution(num1, num2):
+    return int(num1 / num2 * 1000)
